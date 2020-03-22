cc22870b585ffb88a4ba101120ee9202f85863f5
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-22 21:06:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+let element = document.querySelector('main#main');
+element.remove()
+
+let newHeader = document.createElement('h1#victory');
+newHeader.nodeName = 'H1';
+newHeader.id = 'victory';
+newHeader.innerHTML = '<h1>Emil is the champion</h1>'
+
+
+
+
