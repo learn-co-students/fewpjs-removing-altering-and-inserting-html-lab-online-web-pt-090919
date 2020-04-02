4810ca2a7affadd758df65c3dfac53258c3d6516
fasnimo <fasnimo@gmail.com>
2020-04-01 23:01:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code here!
+let remove = document.querySelector('main#main');
+remove.remove('main');
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'Rene is the champion';
+
+
+// for (let i = 0; i < 3; i++) {
+//     let iD = document.createElemnet('id', 'victory')
+//     iD.innerHTML = `Rene is the champion`
+//     newHeader.appendChild(iD)
+// }
+
+// newHeader.appendChild('id', 'victory');
+
+// newHeader.innerHTML = 'Rene is the champion';
