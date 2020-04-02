2816d41cff93f87205f0efe894bcd74cd49bf3c2
fasnimo <fasnimo@gmail.com>
2020-04-01 23:02:06 -0500

    Done.

--- index.js
+++ index.js
@@ -6,14 +6,3 @@ let newHeader = document.createElement('h1');
 newHeader.id = 'victory'
 
 newHeader.innerHTML = 'Rene is the champion';
-
-
-// for (let i = 0; i < 3; i++) {
-//     let iD = document.createElemnet('id', 'victory')
-//     iD.innerHTML = `Rene is the champion`
-//     newHeader.appendChild(iD)
-// }
-
-// newHeader.appendChild('id', 'victory');
-
-// newHeader.innerHTML = 'Rene is the champion';
